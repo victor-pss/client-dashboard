9138fff3e21890c3643430322e5bee14cbc4903b
Victor Alcaraz <vctralcaraz@gmail.com>
2024-08-06 15:15:11 -0700

    updated the card template to have all descriptions to add target blanks to a tags.

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import DOMPurify from 'dompurify';
 
 const Card: React.FC<{ status: string, title: string, id: string, description: string }> = ({ title, description, status }: any) => {
-  const one = description.replace(/<a/gm, '<a target="_blank" rel="noopener"');
+  const descriptionString = typeof description === 'string' ? description : '';
+
+  const modifiedDescription = descriptionString.replace(/<a href=/gmi, '<a target="_blank" href=');
+
+  const sanitizedDescription = DOMPurify.sanitize(modifiedDescription);
+
   return (
-    // <div className="flex flex-col items-center text-gray-900">
-    //   <h2>{title}</h2>
-    //   <p dangerouslySetInnerHTML={{ __html: description }}></p>
-    // </div>
     <div
       className="relative flex flex-col bg-clip-border rounded-xl bg-gradient-to-br from-[#802BB1] to-[#0024C0] text-white shadow-gray-500 shadow-md w-full max-w-[40rem] p-8 mb-4">
       <div
@@ -20,7 +21,7 @@ const Card: React.FC<{ status: string, title: string, id: string, description: s
         </h1>
       </div>
       <div className="p-0">
-        <p className="block font-sans text-base antialiased font-normal leading-relaxed text-inherit" dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(one) }}>
+        <p className="block font-sans text-base antialiased font-normal leading-relaxed text-inherit" dangerouslySetInnerHTML={{ __html: sanitizedDescription }}>
         </p>
       </div>
     </div>
